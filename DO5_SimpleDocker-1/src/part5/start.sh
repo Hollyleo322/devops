047db33857f8c2a5757a2f9129ec918b45f07156
hollyleo_notebook <hollyleonana@gmail.com>
2025-03-19 20:26:03 +0500

    deleted

--- DO5_SimpleDocker-1/src/part5/start.sh
+++
@@ -1,2 +0,0 @@
-spawn-fcgi -p 8080 fcgi_server
-nginx -g "daemon off;"
--- DO5_SimpleDocker-1/src/server/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -Wall -Wextra -Werror -o fcgi_server fcgi.c -lfcgi
-spawn-fcgi -p 8080 fcgi_server
-nginx -g "daemon off;"
